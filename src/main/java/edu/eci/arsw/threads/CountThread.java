97a7e65d08d768330e1ba867a0b464279522ebac
nicolas.ariza241@gmail.com <nicolas.ariza241@gmail.com>
2023-08-23 16:19:38 -0500

    Creada clase

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -7,8 +7,28 @@ package edu.eci.arsw.threads;
 
 /**
  *
- * @author hcadavid
+ * @author NickArB
  */
-public class CountThread {
-    
+public class CountThread extends Thread{
+    private int valueA;
+    private int valueB;
+
+    CountThread(int a, int b){
+        this.valueA=a;
+        this.valueB=b;
+    }
+
+    @Override
+    public void run(){
+        int a = this.valueA;
+        int b = this.valueB;
+        for (int i=a; i <= b; i++){
+            System.out.println(""+i);
+        }
+    }
+
+    public static void main(String[] args){
+        CountThread t = new CountThread(0, 5);
+        t.start();
+    }
 }
